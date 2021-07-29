17e2cc0317776771a7c0567cb21ae608087eefcc
Data James <61133735+KEVIN1996420@users.noreply.github.com>
2021-07-29 10:54:57 +0700

    Completed project, finish firebase setup.

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,20 +1,12 @@
-/* jshint esversion: 9 */
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-// we copy this from the firebase website 
-// https://console.firebase.google.com/u/0/project/vuejs2-blogposts/overview
 var firebaseConfig = {
-    apiKey: "AIzaSyALEgXvOs127Cudjep0UO1ZqRdjaJHEm2w",
-    authDomain: "vuejs2-blogposts.firebaseapp.com",
-    projectId: "vuejs2-blogposts",
-    storageBucket: "vuejs2-blogposts.appspot.com",
-    messagingSenderId: "85528363323",
-    appId: "1:85528363323:web:da0f5440f9826214dcb71e"
-  };
+  // ENTER YOUR CONFIG HERE
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-  export { timestamp };
-  export default firebaseApp.firestore()
+export { timestamp };
+export default firebaseApp.firestore();
